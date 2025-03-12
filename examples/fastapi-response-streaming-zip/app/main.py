1075191036e37074f8b4db00e85963d5a3578efe
katjes <katjes733@gmx.net>
2025-03-12 08:11:02 -0700

    feat: tweaks and logging

--- examples/fastapi-response-streaming-zip/app/main.py
+++ examples/fastapi-response-streaming-zip/app/main.py
@@ -1,14 +1,30 @@
 import boto3
 import json
 import os
+import logging
+import time
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 from typing import Optional
 
+levels = {
+    'critical': logging.CRITICAL,
+    'error': logging.ERROR,
+    'warn': logging.WARNING,
+    'info': logging.INFO,
+    'debug': logging.DEBUG
+}
+logger = logging.getLogger()
+try:
+    logger.setLevel(levels.get(os.getenv('LOG_LEVEL', 'info').lower()))
+except KeyError:
+    logger.setLevel(logging.INFO)
+
 app = FastAPI()
 
+bedrock = boto3.client('bedrock-runtime')
 
 class Story(BaseModel):
     """
@@ -37,9 +53,6 @@ def api_story(story: Story) -> StreamingResponse:
     return StreamingResponse(bedrock_stream(story.topic), media_type="text/html")
 
 
-bedrock = boto3.client('bedrock-runtime')
-
-
 async def bedrock_stream(topic: str):
     """
     Stream the response from the bedrock model based on the given topic.
@@ -49,6 +62,9 @@ async def bedrock_stream(topic: str):
     """
     instruction = f"""
     You are a world class writer. Please write a sweet bedtime story about {topic}.
+    Always end with "The End".
+    Always be polite and friendly.
+    Never add any copyright related information to your answer.
     """
     body = json.dumps({
         "anthropic_version": "bedrock-2023-05-31",
@@ -61,20 +77,32 @@ async def bedrock_stream(topic: str):
         ],
     })
 
+    logger.debug("Invoking model with response stream...")
+    start_time = time.time()
     response = bedrock.invoke_model_with_response_stream(
         modelId='anthropic.claude-3-haiku-20240307-v1:0',
         body=body
     )
+    end_time = time.time()
+    duration = end_time - start_time
+    logger.info("Invoked model with response stream in %.3f seconds", duration)
 
     stream = response.get('body')
+    count = 1
     if stream:
+        logger.debug("Streaming...")
+        start_time = time.time()
         for event in stream:
             chunk = event.get('chunk')
             if chunk:
                 message = json.loads(chunk.get("bytes").decode())
                 if message['type'] == "content_block_delta":
+                    logger.debug("Chunk %d: %s", count, message['delta']['text'])
                     yield message['delta']['text'] or ""
                 elif message['type'] == "message_stop":
+                    end_time = time.time()
+                    duration = end_time - start_time
+                    logger.info("Streaming completed in %.3f seconds", duration)
                     yield "\n"
 
 
